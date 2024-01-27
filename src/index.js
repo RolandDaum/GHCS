4a5181bbd50028f162f66f8ab6b18cdade783183
OMG_OT <monsterroland7@gmail.com>
2024-01-27 23:08:17 +0100

    working. Next: add more accs + webserver

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-let cred = require("./cred.json");
-let CSEnvVar = require("/workspaces/.codespaces/shared/environment-variables.json")
+const cred = require("./cred.json");
+const CSEnvVar = require("/workspaces/.codespaces/shared/environment-variables.json");
 
 main();
 
 async function main() {
-  // await sleep(10000);
+  await sleep(100000);
 
   let currentIndex = cred.GitHubAccs.findIndex(acc => acc.codespace_name === CSEnvVar.CODESPACE_NAME);
-  if (currentIndex++ > cred.GitHubAccs.length) {
-    startstopCodespace(cred.GitHubAccs[0], "start");
+  if (currentIndex + 1 > cred.GitHubAccs.length - 1) {
+    startstopCodespace(0, "start"); // Pass 0 as the index to start from the beginning
   } else {
-    startstopCodespace(cred.GitHubAccs[currentIndex++], "start");
+    startstopCodespace(currentIndex + 1, "start"); // Pass currentIndex + 1 as the index
   }
+
+  await sleep(25000);
+  startstopCodespace(currentIndex, "stop")
 }
 
-/**
- * 
- * @param CODESPACE_NAME String value of the codespace (e.g. probable-bassoon-7vvqx7p4775v2xj49)
- * @param startstop String value to "start", or "stop" a codespace
- */
-function startstopCodespace(CODESPACE_NAME, startstop) {
+function startstopCodespace(newindex, startstop) {
+  console.log(startstop + " - " + newindex);
   if (startstop === "start" || startstop === "stop") {
-    console.log(startstop);
     var headers = new Headers();
     headers.append("accept", "application/vnd.github+json");
     headers.append("X-GitHub-Api-Version", "2022-11-28");
-    headers.append(`Authorization", "Bearer ${cred.GitHubAccs[CODESPACE_NAME].token}`);
+    headers.append("Authorization", `Bearer ${cred.GitHubAccs[parseInt(newindex)].token}`);
     
     var requestOptions = {
       method: 'POST',
@@ -33,13 +31,15 @@ function startstopCodespace(CODESPACE_NAME, startstop) {
       redirect: 'follow'
     };
 
-    fetch(`https://api.github.com/user/codespaces/${CODESPACE_NAME}/${startstop}`, requestOptions)
+    fetch(`https://api.github.com/user/codespaces/${cred.GitHubAccs[parseInt(newindex)].codespace_name}/${startstop}`, requestOptions)
       .then(response => response.json())
       // .then(result => console.log(result))
-      .then(startstopCodespace(CSEnvVar.CODESPACE_NAME, "stop"))
       .catch(error => console.log('error', error));
   } else {
-    return
+    return;
   }
 }
-function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
